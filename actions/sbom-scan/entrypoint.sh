02e45669cbdcc1e6471b164ed9b96ce3eea1f1b6
voolyvex <1joeknapp@gmail.com>
2026-04-28 02:22:52 -0500

    create and edit sbom-scan/Dockerfile and sbom-scan/entrypoint.sh for the applicable user story entry

--- actions/sbom-scan/entrypoint.sh
+++ actions/sbom-scan/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-syft /github/workspace/requirements.txt -o cyclonedx-json=sbom.json
+set -e
+syft /requirements.txt -o cyclonedx-json=sbom.json
 grype sbom.json --by-cve --fail-on critical
-
+cat sbom.json
