f5fd0eeeb54560d1e5120f003dca508e01f69405
voolyvex <1joeknapp@gmail.com>
2026-04-30 17:58:41 -0500

    fix: removed trailing spaces from actions/terraform/entrypoint.sh

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -10,14 +10,14 @@ export DEV_STATE_KEY=${INPUT_DEV_STATE_KEY}
 export TF_STAGE=${INPUT_TF_STAGE}
 export DJANGO_SECRET_KEY=${INPUT_DJANGO_SECRET_KEY}
 
-if [[ "$TF_STAGE" == "stage1   " ]]; then
+if [[ "$TF_STAGE" == "stage1" ]]; then
     terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
     terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
     terraform -chdir=${INPUT_TF_STAGE} apply -auto-approve ${INPUT_TF_STAGE}.tfplan
-elif [[ "$TF_STAGE" == "stage2   " ]]; then
+elif [[ "$TF_STAGE" == "stage2" ]]; then
     terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
     terraform -chdir=${INPUT_TF_STAGE} apply -auto-approve -var="ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}" -var="DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}"
-elif [[ "$TF_STAGE" == "stage3   " ]]; then
+elif [[ "$TF_STAGE" == "stage3" ]]; then
     terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
     terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
     terraform -chdir=${INPUT_TF_STAGE} apply -auto-approve ${INPUT_TF_STAGE}.tfplan
